8b9dbb8c235edcf3a23439e3a405906c5e65241c
Rajukumar122 <er.rajukumar54321@gmail.com>
2025-04-21 23:57:12 +0530

    level_1_doc_add

--- smartidea_backend/settings.py
+++ smartidea_backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -116,6 +116,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# manually add static 
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static"),
+'/var/www/static/',]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
