992d01505f7de9469dc2a20db561ab030b406209
Rajukumar122 <er.rajukumar54321@gmail.com>
2025-04-22 21:32:50 +0530

    level1 part updted and home directory added

--- smartidea_backend/settings.py
+++ smartidea_backend/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'smartidea_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ os.path.join(BASE_DIR, "templates"),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,8 +117,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 # manually add static 
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static"),
-'/var/www/static/',]
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static"),]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
